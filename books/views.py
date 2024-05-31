b51497627813261045b2924c06d8332ca4ed8243
pryanikalenaknu <165229746+pryanikalenaknu@users.noreply.github.com>
2024-05-31 09:35:59 +0300

    Add all authors view

--- books/views.py
+++ books/views.py
@@ -5,3 +5,8 @@ def books(request):
     books = Book.objects.all()
     context = {'title': 'Books', 'books': books}
     return render(request, 'book_list.html', context)
+
+def authors(request):
+    authors = Author.objects.all()
+    context = {'authors': authors}
+    return render(request, 'author_list.html', context)
