ccd333b79cfbd46c47b2983e3594c7e4fc2f2908
pryanikalenaknu <165229746+pryanikalenaknu@users.noreply.github.com>
2024-05-31 09:45:24 +0300

    Add author details to views

--- books/views.py
+++ books/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from books.models import Book, Author
 
-def books(request):
+def book_list(request):
     books = Book.objects.all()
     context = {'books': books}
     return render(request, 'book_list.html', context)
 
-def authors(request):
+def author_list(request):
     authors = Author.objects.all()
     context = {'authors': authors}
     return render(request, 'author_list.html', context)
@@ -15,3 +15,8 @@ def book_details(request, id):
     book = get_object_or_404(Book, id=id)
     context = {'book': book}
     return render(request, 'book_detail.html', context)
+
+def author_details(request, id):
+    author = get_object_or_404(Author, id=id)
+    context = {'author': author}
+    return render(request, 'author_detail.html', context)
