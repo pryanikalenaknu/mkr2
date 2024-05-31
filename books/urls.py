3377bd22e0a2231658f92cc20c5f909b7cf286b7
pryanikalenaknu <165229746+pryanikalenaknu@users.noreply.github.com>
2024-05-31 10:11:28 +0300

    fix urls

--- books/urls.py
+++ books/urls.py
@@ -5,9 +5,9 @@ from books import views
 app_name = 'books'
 
 urlpatterns = [
-    path('books/', views.book_list, name='books'),
-    path('authors/', views.author_list, name='authors'),
-    path('books/book_details/<int:id>', views.book_details, name='book_details'),
-    path('authors/author_details/<int:id>', views.author_details, name='author_details'),
-    path('books/order', views.book_order, name='book_order'),
+    path('books/', views.books, name='books'),
+    path('authors/', views.authors, name='authors'),
+    path('books/book_detail/<int:id>', views.book_detail, name='book_detail'),
+    path('authors/author_detail/<int:id>', views.author_detail, name='author_detail'),
+    path('books/ordered', views.books_ordered, name='books_ordered')
 ]
