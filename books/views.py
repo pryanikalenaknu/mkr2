dcbf1993dc16eae61b1cf569bc616520c3510c4a
pryanikalenaknu <165229746+pryanikalenaknu@users.noreply.github.com>
2024-05-31 09:44:21 +0300

    Add book_details to view

--- books/views.py
+++ books/views.py
@@ -3,10 +3,15 @@ from books.models import Book, Author
 
 def books(request):
     books = Book.objects.all()
-    context = {'title': 'Books', 'books': books}
+    context = {'books': books}
     return render(request, 'book_list.html', context)
 
 def authors(request):
     authors = Author.objects.all()
     context = {'authors': authors}
     return render(request, 'author_list.html', context)
+
+def book_details(request, id):
+    book = get_object_or_404(Book, id=id)
+    context = {'book': book}
+    return render(request, 'book_detail.html', context)
