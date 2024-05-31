8c509078c05f0024284fbd433df3d42bcee9dc75
pryanikalenaknu <165229746+pryanikalenaknu@users.noreply.github.com>
2024-05-31 10:10:05 +0300

    fix views

--- books/views.py
+++ books/views.py
@@ -2,16 +2,18 @@ from django.shortcuts import render
 from books.models import Book, Author
 
 def book_order(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by('-title')
     context = {'books': books}
     return render(request, 'book_list_ordered.html', context)
 
 def book_list(request):
+    Book.objects.create(title="Test1", description="Test", price=12, publication_date=datetime.now(), cover_image="test.png")
     books = Book.objects.all()
     context = {'books': books}
     return render(request, 'book_list.html', context)
 
 def author_list(request):
+    Author.objects.create(name="Test1", bio="Test2")
     authors = Author.objects.all()
     context = {'authors': authors}
     return render(request, 'author_list.html', context)
