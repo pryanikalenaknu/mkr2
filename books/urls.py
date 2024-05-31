e24bfb56325fcdac0f6c3107b4176408e2b86a9b
pryanikalenaknu <165229746+pryanikalenaknu@users.noreply.github.com>
2024-05-31 10:08:33 +0300

    Fix urlrs

--- books/urls.py
+++ books/urls.py
@@ -7,6 +7,7 @@ app_name = 'books'
 urlpatterns = [
     path('books/', views.book_list, name='books'),
     path('authors/', views.author_list, name='authors'),
-    path('book_detail/<int:id>', views.book_details, name='book_detail'),
-    path('author_detail/<int:id>', views.author_details, name='author_detail'),
+    path('books/book_details/<int:id>', views.book_details, name='book_details'),
+    path('authors/author_details/<int:id>', views.author_details, name='author_details'),
+    path('books/order', views.book_order, name='book_order'),
 ]
